ebdb58a39eee1065d820263c66b979b2d9305723
andrei <dam195@yahoo.com>
2025-12-09 14:18:32 +0200

    route protection using proxy.ts

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+const DashboardPage = () => {
+  return (
+    <main className="py-12 flex items-center justify-center flex-col bg-white">
+      <h1 className="text-3xl font-bold text-purple-800 mb-4">
+        Dashboard
+      </h1>
+    </main>
+  );
+};
+
+export default DashboardPage;
